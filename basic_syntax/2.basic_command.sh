8f30a6dbe363f2d082e73ef29d8644f2cfe8edc8
luail <kynia0506@naver.com>
2024-11-28 14:09:32 +0900

    vi 모드 테스트 1 )수정사항1 2) 수정사항2 3) 수정사항3

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -5,3 +5,10 @@ git status
 git add .
 
 git add 특정파일(경로포함)
+
+# commit을 통해 메시지타이틀과 메시지내용을 커밋이력으로 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+# git commit만 하고 엔터 실행시 vi모드
+# 첫줄 : title, 두번쨰줄부터 contents
+git commit
