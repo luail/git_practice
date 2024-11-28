d95f87643365654579bd030523a670993d830b89
luail <kynia0506@naver.com>
2024-11-28 12:25:23 +0900

    ignoretest

--- basic_syntax/1.env_settings.sh
+++ basic_syntax/1.env_settings.sh
@@ -54,3 +54,7 @@ git config --global user.email "유저email"
 # 현재 레파지토리에 한해서만 사용자 변경
 git config --local user.name "유저네임"
 git config --local user.email "유저email"
+
+# 사용자 이름 조회
+git config user.name
+git config user.email
