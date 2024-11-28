0d9f5dad3c579814f21d4b29dd9382a59bdf1ccd
luail <kynia0506@naver.com>
2024-11-28 14:22:07 +0900

    오후수업중 내용

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -15,3 +15,18 @@ git commit
 
 # add와 commit을 동시에
 git commit -am "커밋메시지"
+
+# commit 이력 확인(local repository의 이력)
+git log
+
+# log내용을 1줄로 간결하게
+git log --oneline
+
+# 커밋이력을 그래프 형태로 조회
+git log --graph
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 충돌발생시 무시하고 강제 push
+git push origin main --force
